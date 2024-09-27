832770a1eb58ed11d7cab0533187251fa7e31a09
LDNZ <lucius.dinzeo@gmail.com>
2024-09-27 15:40:40 +0000

    we solved the first task, pat on the back for me

---
+++ homework_1/task_1.m
@@ -0,0 +1,15 @@
+% Author Name: Lucius Dinzeo
+% Email: dinzeo22@students.rowan.edu
+% Created On: 09/18/2024
+% Updated On: 09/20/2024
+% Update By: Dinzeo
+% All rights are reserved - MIT License
+% Title: Homework 1
+
+%%
+radius = 5; %Makes a variable "radius" with a value of 5
+
+area = pi * radius^2; %Makes the variable "area" with a value determined by pi * radius^2
+
+circumference = 2 * pi * radius; %Makes the variable "circumference" with a value also determined by the radius
+
